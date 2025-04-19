3e665653e6a246a6d4557b39371d4f8fdd0748c5
Tested Technologies <testedtechs@gmail.com>
2025-04-19 06:24:05 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from pet import Pet
+
+# Create a new pet
+my_pet = Pet("Buddy")
+
+# Try out the different actions
+my_pet.get_status()
+
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+
+my_pet.train("sit")
+my_pet.train("roll over")
+
+my_pet.show_tricks()
+
+my_pet.get_status()
